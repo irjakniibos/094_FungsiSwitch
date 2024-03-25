5d4e0938a4fbce1fee071dcf10e047c3f1c55ea7
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-03-25 14:56:23 +0700

    memperbaiki display menjadi cout pada line 53

--- calculator.cpp
+++ calculator.cpp
@@ -1,55 +1,56 @@
 #include <iostream>
 using namespace std;
 
-int bilSatu,bilDua;  //membuat variabel global 
+int bilSatu, bilDua; // membuat variabel global
 
-void input (){
-    cout << "Masukan Bilanga Pertama = " ;
-    cin >> bilSatu ;
-    cout << "Masukan Bilangan kedua = " ;
-    cin >> bilDua ;
+void input()
+{
+    cout << "Masukan Bilanga Pertama = ";
+    cin >> bilSatu;
+    cout << "Masukan Bilangan kedua = ";
+    cin >> bilDua;
 }
 
-float penjumlahan(float a,float b)
+float penjumlahan(float a, float b)
 {
-    return a + b ;
+    return a + b;
 }
 
-float pembagian(float a,float b)
+float pembagian(float a, float b)
 {
-    return a / b ;
+    return a / b;
 }
 
-float perkalian(float a,float b)
+float perkalian(float a, float b)
 {
-    return a * b ;
+    return a * b;
 }
 
-float pengurangan(float a,float b)
+float pengurangan(float a, float b)
 {
-    return a - b ;
+    return a - b;
 }
 
 int main()
 {
     int pilihan;
 
-    do{
-        cout << "==================" << endl ;
-        cout << "Menu" << endl ;
-        cout << "Pemjumlahan" << endl ;
-        cout << "Pembagian" << endl ;
-        cout << "Perkalian" << endl ;
-        cout << "Pengurangan" << endl ;
-        cout << "Masukan pilihan 1/2/3/4/5 = " ;
-        cin >> pilihan ;
+    do
+    {
+        cout << "==================" << endl;
+        cout << "Menu" << endl;
+        cout << "Pemjumlahan" << endl;
+        cout << "Pembagian" << endl;
+        cout << "Perkalian" << endl;
+        cout << "Pengurangan" << endl;
+        cout << "Masukan pilihan 1/2/3/4/5 = ";
+        cin >> pilihan;
 
-         switch (pilihan) {
-        case 1:
+        switch (pilihan)
+        {
+        case 1 :
             input();
-            display("Hasil Penjumlahan = ", Penjumlahan(bilSatu,bilDua));
-            system("pause");
+            cout << "hasil penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
             break;
     }
 }
-
