0f0109c4a5b6f7acd2088e42e8f15884eb88243f
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-03-25 14:28:55 +0700

    Membuat Prosedur input

--- calculator.cpp
+++ calculator.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int bilSatu,bilDua;  //membuat variabel global 
 
+void input (){
+    cout << "Masukan Bilanga Pertama = " ;
+    cin >> bilSatu ;
+    cout << "Masukan Bilangan kedua = " ;
+    cin >> bilDua ;
+}
 
 
 
